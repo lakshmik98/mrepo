8b15cc1d8351fea752797b5e2c4e5aa1655cd98b
lakshmik98 <lakshmikonda.neo@gmail.com>
2022-02-13 22:22:18 +0530

    2nd commit

---
+++ Commands.java
@@ -0,0 +1,12 @@
+my git commands
+git init
+   84  git status
+   85  git init
+   86  git status
+   87  git add repo.java
+   88  git status
+   89  git commit -m "first commit"
+   90  git remote add origin "https://github.com/lakshmik98/mrepo.git"
+   91  git remote -v
+   92  git push origin master
+   93  history
